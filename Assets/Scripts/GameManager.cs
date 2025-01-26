22705bf68debe682d60df8baca243d4ed04dd87c
Midnite Run Studios <cybernetikshadow@hotmail.com>
2025-01-25 19:36:26 -0600

    start game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   
+    public GameState GameState;
+    public TMP_Text TMP_Text;
+
+    private void Start()
+    {
+        GameState.isPlaying = false;
+    }
+    private void Update()
+    {
+        if (GameState == null)
+        {
+            return;
+        }
+
+        if (!GameState.isPlaying)
+        {
+            if (Input.anyKey)
+            {
+                GameState.isPlaying = true;
+                TMP_Text.enabled = false;
+            }
+        }
+    }
 }
