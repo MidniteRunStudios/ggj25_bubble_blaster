479bed3bbc55fd8c3885f0b223f0be044796ac37
Midnite Run Studios <cybernetikshadow@hotmail.com>
2025-01-26 01:51:39 -0600

    final touches

--- Assets/Scripts/Effects/TextEffects.cs
+++ Assets/Scripts/Effects/TextEffects.cs
@@ -5,29 +5,12 @@ using TMPro;
 [RequireComponent(typeof(TMP_Text))]
 public class TextEffects : MonoBehaviour
 {
-    
-    public float bounceAmplitude = 1.0f;
-    public float bounceFrequency = 1.0f;
     public float colorChangeSpeed = 1.0f;
-
-    private Vector3 originalPosition;
     private TMP_Text tmpText;
     void Start()
     {
         tmpText = GetComponent<TMP_Text>();
-        originalPosition = tmpText.transform.localPosition;
         StartCoroutine(ChangeColor());
-    }
-
-    void Update()
-    {
-        Bounce();
-    }
-
-    void Bounce()
-    {
-        float newY = Mathf.Sin(Time.deltaTime * bounceFrequency) * bounceAmplitude;
-        tmpText.transform.localPosition = new Vector3(originalPosition.x, originalPosition.y + newY, originalPosition.z);
     }
 
     IEnumerator ChangeColor()
